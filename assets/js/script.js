e9dd4784934d13eda0d3cf20fab00357fd770de0
vomkaraxioned <omkarv@axioned.com>
2022-10-05 13:46:26 +0530

    xmlhttp request object created to get data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
 /* Author: 
-
+omkar valve
 */
+const url = " https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_8a62s5Wi3WavotreFXMViJoZAtCASlUBrg0theNVTlEa5Gzknkzef1549FExxwIT";
+const xmlHttpObj = new XMLHttpRequest();
+xmlHttpObj.onreadystatechange = function () {
+    try {
+        if(this.readyState == 4 && this.status == 200){
+            console.log(JSON.parse(this.responseText));
+        }else {
+            throw this.status + ":"+ this.statusText;        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+xmlHttpObj.open("get",url);
+xmlHttpObj.send();
 
 
 
